82935dc440371085404e08c44c1f7d3ab41e1878
akoruth95 <akoruth95@gmail.com>
2019-01-12 19:34:23 -0500

    fix merge (#32)

--- src/pages/Events/EventDetails.vue
+++ src/pages/Events/EventDetails.vue
@@ -16,15 +16,14 @@
         </v-card-text>
       </v-flex>
     </v-layout>
-    <br>
-    <br>
-    <v-container grid-list-xl>
+
+<v-container grid-list-xl>
       <v-layout row wrap>
         <v-flex xs6 v-for="(option, index) in options" :key="index">
-          <v-card flat color="primary" :to="option.route">
-            <v-flex text-xs-center class="primaryLight elevation-10">
-              <v-icon x-large>{{ option.icon }}</v-icon>
-              <div>{{ option.name}}</div>
+          <v-card class="elevation-5 primary" :to="option.route">
+            <v-flex text-xs-center>
+              <v-icon x-large white right>{{option.icon}}</v-icon>
+              <div>{{option.name}}</div>
             </v-flex>
           </v-card>
         </v-flex>
