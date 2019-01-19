af9fafbfd84ef6c9981ddc0b506d3a4118558542
ausaif@gmail.com <ausaif@gmail.com>
2019-01-19 18:28:35 -0500

    style changes

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,25 +3,30 @@
     <v-btn v-if="showBackButton" fab small color="primary" :to="backlink">
       <v-icon style="height:auto;width:auto" small>fa-chevron-left</v-icon>
     </v-btn>
-    <v-btn v-if="!showBackButton" fab small color="primary" to="/">
+    <!-- <v-btn v-if="!showBackButton && showHomeButton" fab small color="primary" to="/">
       <v-icon style="height:autho; width:auto;" small>fa-home</v-icon>
-    </v-btn>
-    <v-spacer></v-spacer>
+    </v-btn>-->
+    <!-- <v-spacer></v-spacer> -->
     <v-toolbar-title>{{heading}}</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn fab small color="primary" to="/faq">
-      <v-icon style="height:auto;width:auto" small>fa-info</v-icon>
+    <v-btn flat small fab to="/faq">
+      <v-icon style="height:auto;width:auto">fa-info</v-icon>
     </v-btn>
   </v-toolbar>
 </template>
 
 <script>
-  import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
-  export default {
-    computed: {
-      ...mapState('common', ['backlink', 'heading', 'showBackButton']),
-    },
-    name: "TopBar"
-  };
+export default {
+  computed: {
+    ...mapState("common", [
+      "backlink",
+      "heading",
+      "showBackButton",
+      "showButton"
+    ])
+  },
+  name: "TopBar"
+};
 </script>
