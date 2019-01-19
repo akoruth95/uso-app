a477e5a13dbb6079da566895d1b0534c33bd8c80
ausaif@gmail.com <ausaif@gmail.com>
2019-01-19 18:28:35 -0500

    style changes

--- src/store/common.module.js
+++ src/store/common.module.js
@@ -1,36 +1,43 @@
 const state = {
-    backlink: '',
-    heading: '',
-    showBackButton: false
+  backlink: "",
+  heading: "",
+  showBackButton: false,
+  showHomeButton: true
 };
 
 const actions = {
-    setNewBacklink({commit}, link) {
-        commit('newBacklink', link);
-    },
-    setNewHeading({commit}, heading) {
-        commit('newHeading', heading);
-    },
-    setShowBackButton({commit}, show) {
-        commit('showBackButton', show);
-    }
+  setNewBacklink({ commit }, link) {
+    commit("newBacklink", link);
+  },
+  setNewHeading({ commit }, heading) {
+    commit("newHeading", heading);
+  },
+  setShowBackButton({ commit }, show) {
+    commit("showBackButton", show);
+  },
+  setShowHomeButton({ commit }, home) {
+    commit("showHomeButton", home);
+  }
 };
 
 const mutations = {
-    newBacklink(state, link) {
-        state.backlink = link;
-    },
-    newHeading(state, heading) {
-        state.heading = heading;
-    },
-    showBackButton(state, show) {
-        state.showBackButton = show;
-    }
+  newBacklink(state, link) {
+    state.backlink = link;
+  },
+  newHeading(state, heading) {
+    state.heading = heading;
+  },
+  showBackButton(state, show) {
+    state.showBackButton = show;
+  },
+  showHomeButton(state, home) {
+    state.showBackButton = home;
+  }
 };
 
 export const common = {
-    actions,
-    mutations,
-    namespaced: true,
-    state
+  actions,
+  mutations,
+  namespaced: true,
+  state
 };
