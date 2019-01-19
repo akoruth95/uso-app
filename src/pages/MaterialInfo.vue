4a258f715f0f54a0a14fe1c02245c65a0f028ef1
kumarasamyr <kumarasamyr@gmail.com>
2019-01-19 18:32:26 -0500

    Kumar (#40)
    * speaker bio
    
    * material home page added
    
    * material home page added
    
    * material details page added
    
    * error fix

--- src/pages/MaterialInfo.vue
+++ src/pages/MaterialInfo.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container class="text-xs-center" fluid grid-list-lg>
     <h1>{{selectedMaterial.name}}</h1>
+      
+
       <v-avatar size="200px"><v-img :aspect-ratio="16/9" src="https://taj.tajhotels.com/content/dam/luxury/hotels/Taj_Mahal_Delhi/images/4x3/HotelFacade4x3.jpg">
       </v-img></v-avatar>
 
@@ -35,7 +37,10 @@
     }
     
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> Kumar (#40)
 =======
 >>>>>>> Kumar (#40)
 </script>
