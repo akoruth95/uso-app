9556f21e0423670e172203141da3f706cd9f73ff
ausaif@gmail.com <ausaif@gmail.com>
2019-01-19 18:41:58 -0500

    fix merges

--- src/pages/MaterialInfo.vue
+++ src/pages/MaterialInfo.vue
@@ -1,39 +1,42 @@
 <template>
   <v-container class="text-xs-center" fluid grid-list-lg>
     <h1>{{selectedMaterial.name}}</h1>
-      
 
-      <v-avatar size="200px"><v-img :aspect-ratio="16/9" src="https://taj.tajhotels.com/content/dam/luxury/hotels/Taj_Mahal_Delhi/images/4x3/HotelFacade4x3.jpg">
-      </v-img></v-avatar>
+    <v-avatar size="200px">
+      <v-img
+        :aspect-ratio="16/9"
+        src="https://taj.tajhotels.com/content/dam/luxury/hotels/Taj_Mahal_Delhi/images/4x3/HotelFacade4x3.jpg"
+      ></v-img>
+    </v-avatar>
 
-      <h2>
-           {{selectedMaterial.description}}
-      </h2>
-      Download Link: {{ selectedMaterial.downloadLink }}
+    <h2>{{selectedMaterial.description}}</h2>
+    Download Link: {{ selectedMaterial.downloadLink }}
   </v-container>
 </template>
 
 
 <script>
-    import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState } from "vuex";
 
-    export default {
-        
-        created() {
-            this.setNewHeading('Materials Info');
-            this.setShowBackButton(true); 
-            this.setNewBacklink('/eventMaterialHome');
-        },
+export default {
+  created() {
+    this.setNewHeading("Materials Info");
+    this.setShowBackButton(true);
+    this.setNewBacklink("/events/material");
+  },
 
-        computed: {
-            ...mapState({
-                selectedMaterial : state => state.materials.selectedMaterial,
-            }),
-        },
+  computed: {
+    ...mapState({
+      selectedMaterial: state => state.materials.selectedMaterial
+    })
+  },
 
-        methods: {
-            ...mapActions('common', ['setNewHeading', 'setShowBackButton', 'setNewBacklink']),
-        }  
-    }
-    
+  methods: {
+    ...mapActions("common", [
+      "setNewHeading",
+      "setShowBackButton",
+      "setNewBacklink"
+    ])
+  }
+};
 </script>
