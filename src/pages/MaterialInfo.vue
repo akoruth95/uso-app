a06747584753c896bf16934663a5186d747b1633
ausaif@gmail.com <ausaif@gmail.com>
2019-01-30 18:19:26 -0500

    material style and route fixes

--- src/pages/MaterialInfo.vue
+++ src/pages/MaterialInfo.vue
@@ -1,39 +1,43 @@
 <template>
   <v-container class="text-xs-center" fluid grid-list-lg>
-    <h1>{{selectedMaterial.name}}</h1>
-      
-
-      <v-avatar size="200px"><v-img :aspect-ratio="16/9" src="https://taj.tajhotels.com/content/dam/luxury/hotels/Taj_Mahal_Delhi/images/4x3/HotelFacade4x3.jpg">
-      </v-img></v-avatar>
-
-      <h2>
-           {{selectedMaterial.description}}
-      </h2>
-      Download Link: {{ selectedMaterial.downloadLink }}
+    <div class="title">{{selectedMaterial.name}}</div>
+    <v-img class="my-2 mx-3" :src="selectedMaterial.photoLink"></v-img>
+    <div class="body-1">{{selectedMaterial.description}}</div>Download Link:
+    <div>
+      <a class="grey--text" :href="selectedMaterial.downloadLink">Click Here</a>
+    </div>
   </v-container>
 </template>
 
 
 <script>
-    import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState } from "vuex";
 
-    export default {
-        
-        created() {
-            this.setNewHeading(this.event.name);
-            this.setShowBackButton(true); 
-            this.setNewBacklink('material');
-        },
+export default {
+  created() {
+    this.setNewHeading(this.event.name);
+    this.setShowBackButton(true);
+    this.setNewBacklink("material");
+  },
 
-        computed: {
-            ...mapState({
-                selectedMaterial : state => state.materials.selectedMaterial,
-                event: state => state.events.selectedEvent,
-            }),
-        },
+  computed: {
+    ...mapState({
+      selectedMaterial: state => state.materials.selectedMaterial,
+      event: state => state.events.selectedEvent
+    })
+  },
 
-        methods: {
-            ...mapActions('common', ['setNewHeading', 'setShowBackButton', 'setNewBacklink']),
-        }  
-    }
+  methods: {
+    ...mapActions("common", [
+      "setNewHeading",
+      "setShowBackButton",
+      "setNewBacklink"
+    ])
+  }
+};
 </script>
+<style>
+.material-text {
+  background-color: #0077ff3b;
+}
+</style>
