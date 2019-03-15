5e022b69d1b2fa4a38a88c264bdbb0407bc0a385
ausaif@gmail.com <ausaif@gmail.com>
2019-03-14 21:40:45 -0400

    fix phonegap settings

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
   api: {
-    sessions: "https://api.v2.sessions.usoncevents.com",
+    // sessions: "https://api.v2.sessions.usoncevents.com",
     events: "https://api.v2.events.usoncevents.com",
-    users: "https://api.v2.users.usoncevents.com",
-    url: "https://api.v1.usoncevents.com/api"
+    // users: "https://api.v2.users.usoncevents.com",
+    url: "https://api.v1.uat.usoncevents.com/api"
   },
   sessionDetails: {
     source: "SESSION"
